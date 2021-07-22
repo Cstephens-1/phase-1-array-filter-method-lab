9ac14bb899935cb42534ff825d728e0633976c1c
Cory <corystephens012@gmail.com>
2021-07-22 13:11:54 -0400

    commit tree e2420996416e98011af4918cab028feb98349ab6

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(array, string){
+    const matchName = array.filter(function(element){
+        return string.toLowerCase() === element.toLowerCase()
+    })
+        return matchName
+}
+
+function fuzzyMatch(array, string){
+    const closeMatch = array.filter(function(element){
+        return string.substring(0,1) === element.substring(0,1)
+    })
+        return closeMatch
+}
+
+
+function matchName(array, string){
+    const matching = array.filter(function(element){
+        return string === element.name
+    })
+    return matching
+}
+
+
+
